a52d46ea99ac3a961c9e8fb6dc5fb6ef6293dc9f
MSavioC <dogehkinn@gmail.com>
2024-12-02 16:50:02 -0300

    first commit

--- taskmasterapp/urls.py
+++ taskmasterapp/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),
 
-    path('list/', views.TaskList_view, name='tasks'),
+    path('taskregister/', views.registrotarefas_view, name='tasksregister'),
 
-    path('registro/', views.registro_view, name='cadastrar'),
+    path('registro/', views.registro_view, name='registro'),
 
-    path('tarefas/', views.registrotarefas_view, name='tarefas'),
+    path('tarefas/', views.TaskList_view, name='tarefas'),
 ]
--- taskmasterapp/views.py
+++ taskmasterapp/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Task
 
 def login_view(request):
     if request.method == "POST":
@@ -10,7 +13,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect('list/')
+            return redirect('tarefas/')
         else:
             return render(request, 'login/index.html', {'error': 'Credenciais invalidas'})
     
@@ -18,10 +21,47 @@ def login_view(request):
     return render(request, 'login/index.html')
 
 def TaskList_view(request):
-    return render(request, 'TaskList/tasks.html')
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, "tasklist/tasklist.html", {"tasks": tasks})
+
+
 
 def registro_view(request):
-    return render(request, 'registro/Cadastrar.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if password != confirm_password:
+            messages.error(request, "As senhas nao coincidem")
+            return redirect("registro")
+       
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            messages.success(request, "Usuario registrado com sucesso! Faça login.")
+            return redirect("login")
+        except Exception as e:
+            messages.error(request, f"Erro ao registrar o usuario: {e}")
+            return redirect ("registro")
+    return render(request, 'registro/cadastrar.html')
 
 def registrotarefas_view(request):
-    return render(request, 'registrotarefas/registrotarefas.html')
+    if request.method == "POST":
+        # OBTEM OS DADOS DO FORMULARIO
+        title = request.POST.get("title")
+        due_date = request.POST.get("due_date")
+        priority = request.POST.get("priority")
+
+        #cria a tarefa e associa ao usuario logado
+        Task.objects.create(
+            user=request.user, #associando ao usuario logado
+            title=title,
+            due_date=due_date,
+            priority=priority,
+        )
+        return redirect("tarefas")
+    return render(request, "taskregister/tasksregister.html")
+
+  
